2c66d209d965e778447dac7deff302ad308d58f1
Todd Lang <todd.lang@d2l.com>
2023-11-28 14:48:52 -0500

    Adding IFileSystemIdentifier constraint

--- src/Kiyote/Files/Kiyote.Files/IFileSystemFactory.cs
+++ src/Kiyote/Files/Kiyote.Files/IFileSystemFactory.cs
@@ -1,8 +1,8 @@
 ﻿namespace Kiyote.Files;
 
 public interface IFileSystemFactory {
-	IReadOnlyFileSystem<T> Create<T>( IReadOnlyFileSystem fileSystem );
+	IReadOnlyFileSystem<T> Create<T>( IReadOnlyFileSystem fileSystem ) where T: IFileSystemIdentifier;
 
-	IReadWriteFileSystem<T> Create<T>( IReadWriteFileSystem fileSystem );
+	IReadWriteFileSystem<T> Create<T>( IReadWriteFileSystem fileSystem ) where T: IFileSystemIdentifier;
 
 }

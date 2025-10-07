6eee519c132ffbf7ae0dafa4fec98078a5679b66
Todd Lang <todd.lang@d2l.com>
2025-10-07 09:50:07 -0400

    Upgrading to net90 (#86)

--- test/Kiyote/Files/Kiyote.Files.UnitTests/GlobalSuppressions.cs
+++ test/Kiyote/Files/Kiyote.Files.UnitTests/GlobalSuppressions.cs
@@ -7,3 +7,4 @@ using System.Diagnostics.CodeAnalysis;
 
 [assembly: SuppressMessage( "Naming", "CA1707:Identifiers should not contain underscores", Justification = "Test names are METHOD_CONDITION_EXPECTATION", Scope = "module" )]
 [assembly: SuppressMessage( "Style", "IDE0130:Namespace does not match folder structure", Justification = "Testing namespaces match as best as possible.", Scope = "module" )]
+[assembly: SuppressMessage( "Maintainability", "CA1515:Consider making public types internal", Justification = "Code not released, doesn't mattter.", Scope = "module" )]
